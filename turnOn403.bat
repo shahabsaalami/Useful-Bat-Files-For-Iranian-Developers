2135e5580402680771dd36627e356d5bb54ff0ae
Shahab Pc Innovation <Shab.salami@gmail.com>
2024-08-27 18:20:21 +0330

    refactor bat file, to get dns data from variables

--- turnOn403.bat
+++ turnOn403.bat
@@ -1,10 +1,32 @@
-netsh interface ip set dnsservers name="Ethernet" source=dhcp
-netsh interface ip set dnsservers name="Wi-Fi" source=dhcp
-netsh interface ipv4 add dnsservers "Ethernet" address=10.202.10.202 index=1
-netsh interface ipv4 add dnsservers "Ethernet" address=10.202.10.102 index=2
-netsh interface ipv4 add dnsservers "Wi-Fi" address=10.202.10.202 index=1
-netsh interface ipv4 add dnsservers "Wi-Fi" address=10.202.10.102 index=2
+@echo off
 
+REM Define DNS addresses
+set primaryDNS=10.202.10.102
+set secondaryDNS=10.202.10.202
+
+echo.
+echo  ----------------------------------------------------
+echo  -  DNS has been altered to 403.online!  			 -
+echo  -  primaryDNS:	%primaryDNS%  					 -
+echo  -  secondaryDNS:	%secondaryDNS%  				 -
+echo  -  Proceed with caution, for magic awaits.  		 -
+echo  ----------------------------------------------------
+echo.
+
+REM Change DNS for all network interfaces
+
+REM For Wi-Fi
+netsh interface ip set dns name="Wi-Fi" static %primaryDNS%
+netsh interface ip add dns name="Wi-Fi" %secondaryDNS% index=2
+
+REM For LAN (Ethernet)
+netsh interface ip set dns name="Ethernet" static %primaryDNS%
+netsh interface ip add dns name="Ethernet" %secondaryDNS% index=2
+
+REM Flush DNS cache
 ipconfig /flushdns
+
+echo DNS servers set to %primaryDNS% and %secondaryDNS% for Wi-Fi and Ethernet.
+echo DNS cache flushed.
 
 pause
